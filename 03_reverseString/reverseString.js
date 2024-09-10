2291354d85415ea825de54a85817ada8898ab22d
David <davkh10@gmail.com>
2024-09-10 12:38:36 -0300

    Give so

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let cars = text.split("")
+    let reversedCars = cars.reverse()
+    return cars.join("")
 };
 
 // Do not edit below this line
