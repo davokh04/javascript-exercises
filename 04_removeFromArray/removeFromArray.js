8cc4e00f6b763b3283da48ceffa65182dd8d96bb
David <davkh10@gmail.com>
2024-09-10 12:39:09 -0300

    Give solution to each exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    let filteredArray = array.filter((item) => item != remove)
+    return filteredArray
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    
+    while ((a > 0) && (b > 0) && Number.isInteger(a) && Number.isInteger(b)) {
+        if (a > b) {
+            let c = b - 1
+            let result = (a * (a + 1) / 2) - (c * (c + 1) / 2)
+            return result
+        } else {
+            let c = a - 1
+            let result = (b * (b + 1) / 2) - (c * (c + 1) / 2)
+            return result
+        }
+    }
+    return "ERROR"
+};npm test 05_sumAll/sumAll.spec.js 
 
 // Do not edit below this line
 module.exports = sumAll;
