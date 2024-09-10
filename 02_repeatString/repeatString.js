94d1f866c9acabc71e90a974dda4ac4dccd167e2
David <davkh10@gmail.com>
2024-09-10 10:04:15 -0300

    Give solution to exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, repetition) {
+    if (repetition == 0) {
+        return ""
+    } else if (repetition < 0) {
+        return "ERROR"
+    }
+    let i = 0;
+    let text = ""
+    do {
+        text += word
+        i++
+    } while (i < repetition);
+    return text
 };
 
 // Do not edit below this line
