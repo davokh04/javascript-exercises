2b45fe4d2f2782622e26c1150a062acd5c1d14ae
David <davkh10@gmail.com>
2024-11-19 13:52:04 -0300

    Solve ex 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => getAge(a) > getAge(b) ? -1 : 1);
+    return people[0];
+};
 
+function getAge(person) {
+    const currentYear = new Date().getFullYear();
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return currentYear - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
