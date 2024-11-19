af0ee044e6dfca5ab593a712eef27d56c5e60c35
David <davkh10@gmail.com>
2024-11-19 13:23:03 -0300

    Solve ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if (pos == 0) {
+        return 0;
+    } else if (pos < 0) {
+        return 'OOPS';
+    }
+    let numbers = [0, 1];
+    for (let i = 2; i < parseInt(pos) + 1; i++) {
+        numbers.push(numbers[i - 1] + numbers[i - 2]);
+    }
+    return numbers.pop();
 };
 
 // Do not edit below this line
