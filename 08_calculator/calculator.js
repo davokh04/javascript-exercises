c0f375e34aeac6e265c00d69b596b2aa9a1342cc
David <davkh10@gmail.com>
2024-11-19 11:46:46 -0300

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let result = nums.reduce((total, num) => total + num);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+	let result = nums.reduce((total, num) => total - num);
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length === 0) {
+    return 0;
+  } else {
+    let result = nums.reduce((total, num) => total + num);
+    return result;
+  }
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let result = nums.reduce((total, num) => total * num);
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
