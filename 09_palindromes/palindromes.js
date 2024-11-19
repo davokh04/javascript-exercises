9e64e971e6ebd12cb4258c8dd4b1fa04d93dc505
David <davkh10@gmail.com>
2024-11-19 12:13:16 -0300

    Solve ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.split('').filter(char => /^[a-zA-Z0-9]+$/.test(char)).join('').toLowerCase();
+    let reversedWord = word.split('').filter(char => /^[a-zA-Z0-9]+$/.test(char)).reverse().join('').toLowerCase();
+    if (word == reversedWord) {
+        return true;
+    } return false;
 };
 
 // Do not edit below this line
